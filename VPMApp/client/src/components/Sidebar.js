09ef1459db1426ad234cf689a1ae63d76c87c0b4
AS <ava.stromme@wsu.edu>
2022-10-18 16:08:25 -0700

    Added a navigation bar and sidebar

---
+++ VPMApp/client/src/components/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import SettingsIcon from '@mui/icons-material/Settings';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import FolderIcon from '@mui/icons-material/Folder';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+    return (
+        <Box sx={{ overflow: 'auto' }}>
+            <List>
+                <ListItem key="Projects" disablePadding >
+                    <ListItemButton component={Link} to="/projects">
+                        <ListItemIcon sx={{ ml: 1, color: 'white' }}> <FolderIcon/> </ListItemIcon>
+                        <ListItemText sx={{ color: 'white' }}> Projects </ListItemText>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem key="Projects" disablePadding >
+                    <ListItemButton>
+                        <ListItemIcon sx={{ ml: 1, color: 'white' }}> <InventoryIcon/> </ListItemIcon>
+                        <ListItemText sx={{ color: 'white' }}> Archive </ListItemText>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem key="Projects" disablePadding >
+                    <ListItemButton>
+                        <ListItemIcon sx={{ ml: 1, color: 'white' }}> <SettingsIcon /> </ListItemIcon>
+                        <ListItemText sx={{ color: 'white' }}> Settings </ListItemText>
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
+  };
+  
+  export default Sidebar;
--- VPMApp/client/src/pages/ProjectHomePage.js
+++ VPMApp/client/src/pages/ProjectHomePage.js
@@ -1,22 +1,12 @@
 import React from 'react'
-import { Button, Toolbar, Typography, Box } from '@mui/material'
-import { Link } from 'react-router-dom'
 import FeverChart from '../components/FeverChart'
+import Navigation from '../components/Navigation'
 
 const ProjectHomePage = () => {
     return (
         <div>
-            <Box sx={{ flexGrow: 1 }}>
-                <Toolbar>
-                    <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-                        Virtual Project Management
-                    </Typography>
-                    <Button component={Link} to="/" size="medium" variant="contained">
-                        Log Out
-                    </Button>
-                </Toolbar>
-            </Box>
-            <div style={{ paddingTop: '25px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Navigation/>
+            <div style={{ paddingLeft: '200px', paddingTop: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <div class="chart-container" style={{ position: 'relative', height: '20vh', width: '40vw' }}>
                     <FeverChart/>
                 </div>
