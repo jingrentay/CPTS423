2c6a000341b46d26bd4d6e6f464869263726ff52
AS <ava.stromme@wsu.edu>
2022-10-25 15:52:25 -0700

    Fixes for routes and controllers for projects on the server side

--- VPMApp/server/models/task.js
+++ VPMApp/server/models/task.js
@@ -1,15 +1,10 @@
 import mongoose from "mongoose";
 
 const taskSchema = mongoose.Schema({
-    projectName: String,
+    taskProjectName: String,
+    taskProjectID: String,
     taskName: String,
     taskDescription: String,
-    duration: Number,
-    assignee: String,
-    completionDate: {
-        type: Date,
-        default: new Date()
-    },
 });
 
 const Task = mongoose.model('Task', taskSchema);
