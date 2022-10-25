af7edc23aa893d032bf76b7279addd66ea929ce7
AS <ava.stromme@wsu.edu>
2022-10-25 15:54:42 -0700

    Bug fix for server

--- VPMApp/server/index.js
+++ VPMApp/server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/projects', projectRoutes);
 
-const CONNECTION_URL= 'mongodb+srv://ava:ava8668@cluster0.fhjhldy.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL= 'mongodb+srv://username:password@cluster0.fhjhldy.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
