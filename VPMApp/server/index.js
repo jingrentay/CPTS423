be17c97fc89e068b816242b4f6ff6343c92c045a
jingrentay <jingren.tay@wsu.edu>
2022-11-07 14:15:02 -0800

    Change the connection_url to env file.

--- VPMApp/server/index.js
+++ VPMApp/server/index.js
@@ -15,9 +15,9 @@ app.use(cors());
 
 app.use('/projects', projectRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://jingrentay:durian123@cluster0.fhjhldy.mongodb.net/?retryWrites=true&w=majority'
+
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
     .catch((error)=> console.log(error.message));
 
