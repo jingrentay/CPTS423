3e77564c635a7e60b745a17e791ca90825ec7ca6
pragunkalra <pragun.kalra@wsu.edu>
2023-01-30 20:02:09 -0800

    predicted completion

--- VPMApp/client/src/utils.js
+++ VPMApp/client/src/utils.js
@@ -1,20 +1,21 @@
 export function getDate(value, name) {
+    
     const currentDate = new Date();
     let newDate = "";
     if (name == "Weeks") {
       return (newDate = new Date(
-        currentDate.getTime() + value * 7 * 24 * 60 * 60 * 1000
+        currentDate.getTime() + (value*1.5) * 7 * 24 * 60 * 60 * 1000
       ));
     }
     if (name == "Days") {
       return (newDate = new Date(
-        currentDate.getTime() + value * 24 * 60 * 60 * 1000
+        currentDate.getTime() + (value*1.5) * 24 * 60 * 60 * 1000
       ));
     }
     if (name == "Hours") {
-      return new Date(currentDate.getTime() + value * 60 * 60 * 1000);
+      return new Date(currentDate.getTime() + (value*1.5) * 60 * 60 * 1000);
     }
     if (name == "Minutes") {
-      return new Date(currentDate.getTime() + value * 60 * 1000);
+      return new Date(currentDate.getTime() + (value*1.5) * 60 * 1000);
     }
 }
