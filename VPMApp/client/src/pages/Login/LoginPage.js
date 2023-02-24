2296d0478d7c2003449fa5aed7fe4388828134d5
AS <ava.stromme@wsu.edu>
2023-02-24 12:00:16 -0800

    Changes to login page

--- VPMApp/client/src/pages/Login/LoginPage.js
+++ VPMApp/client/src/pages/Login/LoginPage.js
@@ -1,47 +1,85 @@
-import React from 'react'
-import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Button, TextField, InputAdornment, IconButton, Grid } from '@mui/material'
+import { VisibilityOff, Visibility } from '@mui/icons-material'
+import { useNavigate } from 'react-router-dom'
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LockIcon from '@mui/icons-material/Lock';
+
 import profile from "../Login/assets/profile.jpeg";
 import email from "../Login/assets/email.jpg";
 import pass from "../Login/assets/password.png";
+
 import "../Login/LoginPage.css";
 
 function LoginPage() {
+
+    const navigate = useNavigate()
+
+    const [accountInfo, setAccountInfo] = useState({ email: '', password: ''})
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowPassword = () => setShowPassword((show) => !show)
+
+    const handleLogin = () => {
+        navigate('/projects')
+    }
+
     return (
-        
-            <div className='main'>
+        <div className='main'>
             <div className="sub-main">
-            <div>
-            <div className="imgs">
-                <div className='container-image'>
-                <img src={profile} alt="profile" className="profile"/>
+                <div style={{ width: '60%' }}>
+                    <div className="imgs">
+                        <div className='container-image'>
+                        <img src={profile} alt="profile" className="profile"/>
+                        </div>
+                    </div>
+                    <div>
+                        <h1>Log In</h1>
+                        <div>
+                            <TextField defaultValue={accountInfo.email} variant='outlined' label='Email' 
+                                style={{ paddingBottom: '15px', display: 'flex', justifyContent: 'center'}} 
+                                onChange={(e) => setAccountInfo({ ...accountInfo, email: e.target.value })}
+                                InputProps={{
+                                    startAdornment: (<InputAdornment position="start"> <MailOutlineIcon/> </InputAdornment>)
+                                }}
+                            />
+                        </div>
+                        <div className="second-input">
+                            <TextField 
+                                defaultValue={accountInfo.password} 
+                                style={{ paddingBottom: '15px', display: 'flex', justifyContent: 'center'}} 
+                                variant='outlined' 
+                                label='Password' 
+                                type={showPassword ? "text" : "password"}
+                                onChange={(e) => setAccountInfo({ ...accountInfo, password: e.target.value })}
+                                InputProps={{
+                                    endAdornment: (<InputAdornment position='end'> 
+                                        <IconButton
+                                            edge='end'
+                                            onClick={handleShowPassword}
+                                        >
+                                        {showPassword ? <VisibilityOff/> : <Visibility/>} 
+                                        </IconButton>
+                                    </InputAdornment>),
+                                    startAdornment: (<InputAdornment position="start"> <LockIcon/> </InputAdornment>)
+                                }}
+                            /> 
+                        </div>
+                        <div className="login-button">
+                            <Button onClick={handleLogin} size="medium" variant="contained">
+                                Log in
+                            </Button>
+                        </div>
+                        <p className="link">
+                            <a href="/signup"> Don't have an account? Sign up </a> 
+                        </p>
+                        <p className="link">
+                            <a href="/"> Forgot password? </a> 
+                        </p>
+                    </div>
                 </div>
-                </div>
-        
-                <div>
-                <h1>Login Page</h1>
-                <div>
-                <img src={email} alt="email" className="email"/>
-                <input type="text" placeholder="user name" className="name"/>
-                </div>
-                <div className="second-input">
-                <img src={pass} alt="pass" className="email"/>
-                <input type="password" placeholder="user name" className="name"/>
-                </div>
-                <div className="login-button">
-                <Button component={Link} to="/projects" size="medium" variant="contained" >
-                Login
-               </Button>
-               </div>
-               
-               <p className="link">
-                <a href="/">Forgot password ?</a> Or<a href="/signup">  Sign Up</a>
-              </p>
-            
             </div>
-            </div>
-            </div>
-            </div>
+        </div>
     );
 }
 
